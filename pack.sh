272d84a3901bff5f06c1a8f58a1563d197ff0640
abj1210 <87817992+abj1210@users.noreply.github.com>
2024-01-22 16:22:54 +0800

    final

--- pack.sh
+++ pack.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-NAME=211220091-闻嘉迅.zip
+NAME=211220091-闻嘉迅
 
-rm -f ${NAME}
-cd Code
-make clean 
-cd ../
-zip -r ${NAME} ./turing-project/* ./programs/case1.tm ./programs/case2.tm CMakeLists.txt README.md
+rm -f ${NAME}.zip
+zip -r ${NAME} ./turing-project/* ./programs/case1.tm ./programs/case2.tm CMakeLists.txt README.md ${NAME}.pdf 
